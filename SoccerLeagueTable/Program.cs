ebb80e78d6fec5ff652c23865d0afa704a01b6c4
Curtis Weide <curtis.weide@gmail.com>
2022-09-11 14:36:32 -0500

    Updated to use regex

--- SoccerLeagueTable/Program.cs
+++ SoccerLeagueTable/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
+
 namespace SoccerLeagueTable
 {
     class Program
@@ -12,7 +14,7 @@ namespace SoccerLeagueTable
             int forCol = 43;
             int againstCol = 50;
             string teamName = "";
-            double? teamDiff = null;
+            double? teamDiff;
             double forTotal;
             double againstTotal;
             double? currentPointsDiff = null;
@@ -21,22 +23,25 @@ namespace SoccerLeagueTable
             for (int i = 1; i < soccerArray.Length - 1; i++)
             {
 
-                if (Double.TryParse(soccerArray[i].Substring(forCol, 2), out forTotal))
+                if(Regex.Match(soccerArray[i].Substring(forCol, 2), @"^\-+$").Success)
                 {
-                    forTotal = Convert.ToDouble(soccerArray[i].Substring(forCol, 2));
-                    againstTotal = Convert.ToDouble(soccerArray[i].Substring(againstCol, 2));
-                    teamDiff = forTotal - againstTotal;
-                    if (teamDiff < 0)
-                    {
-                        teamDiff = teamDiff * -1;
-                    }
+                    continue;
+                }
 
-                    if (currentPointsDiff == null || teamDiff < currentPointsDiff)
-                    {
-                        currentPointsDiff = teamDiff;
-                        teamName = soccerArray[i].Substring(teamNameCol, 14);
-                    }
-                } 
+                forTotal = Convert.ToDouble(soccerArray[i].Substring(forCol, 2));
+                againstTotal = Convert.ToDouble(soccerArray[i].Substring(againstCol, 2));
+                teamDiff = forTotal - againstTotal;
+                if (teamDiff < 0)
+                {
+                    teamDiff = teamDiff * -1;
+                }
+
+                if (currentPointsDiff == null || teamDiff < currentPointsDiff)
+                {
+                    currentPointsDiff = teamDiff;
+                    teamName = soccerArray[i].Substring(teamNameCol, 14);
+                }
+                
             }
 
             Console.WriteLine("The team with the fewest points differential was: " + teamName);
